0c8135eae46d6e03c9a1f46e055b9051f27a870e
Batuhan <batuhan.fidan@ogr.iu.edu.tr>
2022-03-13 16:07:26 +0300

    amazon clone

---
+++ src/Header.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import './Header.css';
+import { Link} from "react-router-dom";
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import { useStateValue } from './StateProvider';
+
+
+function Header() {
+    const [ { basket }] = useStateValue();
+
+    console.log(basket);
+
+
+  return (
+    <nav className="header">
+        <Link to="/">
+        <img 
+            className="header__logo" 
+            src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" 
+            alt=""
+        />
+        </Link>
+
+        <div className="header__search">
+        <input type="text" className="header__searchInput" />
+        <SearchIcon className="header__searchIcon" />
+        </div>
+
+        <div className="header__nav">
+        <Link to="/login" className="header__link">
+            <div className="header__option">
+                <span className="header__optionLineOne" >Hello Batuhan</span>
+                <span className="header__optionLineTwo" >Sign In</span>
+            </div>
+        </Link>
+
+        <Link to="/" className="header__link">
+            <div className="header__option">
+                <span className="header__optionLineOne" >Returns</span>
+                <span className="header__optionLineTwo" >& Orders</span>
+            </div>
+        </Link>
+
+        <Link to="/" className="header__link">
+            <div className="header__option">
+                <span className="header__optionLineOne" >Your</span>
+                <span className="header__optionLineTwo" >Prime</span>
+            </div>
+        </Link>
+
+        <Link to="/checkout" className="header__link" >
+            <div className="header__optionBasket">
+                <ShoppingBasketIcon />
+                <span className="header__optionLineTwo header__basketCount" >{basket?.length} </span>
+            </div>
+        </Link>
+        </div>
+    </nav>
+  )
+}
+
+export default Header
+
+
+
+
